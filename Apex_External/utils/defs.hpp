cd5041cef98e2103f8469d8213fcc047b2a573c7
C0WN0W <imcownow@yeah.net>
2025-09-25 17:30:51 +0800

    created defines

---
+++ Apex_External/utils/defs.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include "structs.hpp"
+
+inline const char* ProcessName = "r5apex_dx12.exe";
+inline const Vector2 ScreenSize = { 1920.f, 1080.f };
--- Apex_External/utils/offsets.hpp
+++ Apex_External/utils/offsets.hpp
@@ -1,9 +1,9 @@
 #pragma once
 #include <cstdint>
 
-const uint64_t OFF_ENTITYLIST = 0x6282C28;
-const uint64_t OFF_VIEW_MATRIX = 0x11A350;
-const uint64_t OFF_VIEW_RENDER = 0x3D3CD78;
-const uint64_t OFF_VecAbsOrigin = 0x17C;
-const uint64_t OFF_iHealth = 0x324;
-const uint64_t OFF_iMaxHealth = 0x468;
+inline uint64_t OFF_ENTITYLIST = 0x6282C28;
+inline uint64_t OFF_VIEW_MATRIX = 0x11A350;
+inline uint64_t OFF_VIEW_RENDER = 0x3D3CD78;
+inline uint64_t OFF_VecAbsOrigin = 0x17C;
+inline uint64_t OFF_iHealth = 0x324;
+inline uint64_t OFF_iMaxHealth = 0x468;
