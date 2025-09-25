ce5b565cfef6ab1493289ef0deaac831484e331f
C0WN0W <imcownow@yeah.net>
2025-09-25 21:17:27 +0800

    init

--- Apex_External/main.cpp
+++ Apex_External/main.cpp
@@ -11,4 +11,35 @@
 int main()
 {
 	SetConsoleTitle(L" ");
+
+	kInterface = new Interface("NtGdiMakeInfoDC");
+
+	if (!kInterface->ConnectToDriver())
+	{
+		std::cout << "[-] Failed To Call Driver\n";
+		Sleep(2000);
+		return EXIT_FAILURE;
+	}
+
+	localPid = PID(L"r5apex_dx12.exe");
+	if (!localPid) {
+		std::cout << "[-] Failed to get PID!";
+		Sleep(2000);
+		return EXIT_FAILURE;
+	}
+	std::cout << "[+] PID: " << localPid << "\n";
+	DWORD64 BaseAddress = kInterface->BaseAddress(localPid, "r5apex_dx12.exe");
+	if (!BaseAddress) {
+		std::cout << "[-] Failed to get base address!\n";
+		system("pause");
+		exit(0);
+	}
+	std::cout << "[+] Base Address: " << std::hex << BaseAddress << "\n";
+
+	gRender = new Render();
+	if (!gRender->InitializeOverlay()) {
+		std::cout << "[-] Failed to initialize overlay!\n";
+		system("pause");
+		exit(0);
+	}
 }
